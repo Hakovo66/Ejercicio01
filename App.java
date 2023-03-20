f18f227313a48b254b44a80788b5fef6c26aef2c
Hakovo66 <113375465+Hakovo66@users.noreply.github.com>
2023-03-19 20:13:53 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+ suma(5, 5, 5);
+
+    }
+    public static void suma (int a, int b, int c){
+    
+        int resultado = a + b + c;
+        System.out.println(resultado);
+        
+        }
+}
+
+
+
